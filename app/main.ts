6268833215240789d49678d6169cbd9efd32302a
Memelli <julio.memelli@gmail.com>
2024-10-18 15:55:58 -0300

    [any message]

--- app/main.ts
+++ app/main.ts
@@ -23,8 +23,6 @@ const bencodedValue = args[3];
 
 if (args[2] === "decode") {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    console.log("Logs from your program will appear here!");
-
     // Uncomment this block to pass the first stage
     try {
         const decoded = decodeBencode(bencodedValue);
